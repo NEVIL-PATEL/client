b14deda0e04d02c11e92fcc95643b1166d63fa26
Nevil Patel <93375041+NEVIL-PATEL@users.noreply.github.com>
2023-02-16 01:04:37 +0530

    responsive nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,37 +1,48 @@
-import React from 'react'
-import {Outlet} from 'react-router-dom'
+import React,{useState} from 'react'
+import { Outlet } from 'react-router-dom'
+// import { GiHamburgerMenu } from "react-icons/gi";
+
 function Header() {
-    return ( 
+    const [showMediaIcons, setShowMediaIcons] = useState(false);
+
+    return (
         <>
-        <div id="header">
-            <nav className="main-nav">
-                <div className="logo">
-                    <h2>
-                        <span>nevil</span>
-                    </h2>
-                </div>
-                <div className="menu-link">
-                    <ul>
-                        <li><a href="">home</a></li>
-                        <li><a href="">about</a></li>
-                        <li><a href="">contact</a></li>
-                        <li><a href="">feed</a></li>
-                        <li><a href="">post</a></li>
-                    </ul>
-                </div>
-                <div className="social-media">
+            <div id="header">
+                <nav className="main-nav">
+                    <div className="logo">
+                        <h2>
+                            <span>nevil</span>
+                        </h2>
+                    </div>
+                    <div className="menu-link">
+                        <ul>
+                            <li><a href="">home</a></li>
+                            <li><a href="">about</a></li>
+                            <li><a href="">contact</a></li>
+                            <li><a href="">feed</a></li>
+                            <li><a href="">post</a></li>
+                        </ul>
+                    </div>
+                    <div className="social-media">
                         <ul className="social-media-desktop">
                             <li><a href="">1</a></li>
                             <li><a href="">2</a></li>
                             <li><a href="">3</a></li>
                         </ul>
-                </div>
-            </nav>
-            
-        </div>
-        <Outlet/>
-        </> 
-     );
+                        {/* hamburget menu start  */}
+                        <div className="hamburger-menu">
+                            <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+                                {/* <GiHamburgerMenu /> */}
+                                <span>-</span>
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+
+            </div>
+            <Outlet />
+        </>
+    );
 }
 
 export default Header;
